89d078360e7f0e73c37aabfd9fc8e3bdc94160c8
kenwrites <kenwrites@users.noreply.github.com>
2019-03-06 11:16:36 -0500

    skeleton for wikipedia API call

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+var url = 'https://en.wikipedia.org/w/api.php?action=parse&origin=*&page=Pet_door&prop=text&section=0&format=json
+var wiki_request = new XMLHttpRequest();
+
+wiki_request.onreadystatechange = ()=>{
+    if (wiki_request.readyState === 4 && wiki_request.status === 200) {
+        console.log(wiki_request)
+    }
+}
+
+wiki_request.open('GET', url)
+wiki_request.send()
 
