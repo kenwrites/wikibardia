1f691f89f44b5626d2823bdd26c3a4128ab58461
kenwrites <kenwrites@users.noreply.github.com>
2019-03-07 09:40:40 -0500

    Finish FunTranslations API call / listener, and post translated text to shakes-output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 const wiki_section = document.querySelector('#wiki-getter')
 const wiki_output = document.querySelector('#wiki-output')
+const shakes_output = document.querySelector('#shakes-output')
 
 var wikipedia_url = 'https://en.wikipedia.org/w/api.php?action=parse&origin=*&page=Pet_door&prop=text&section=0&format=json'
 var wiki_request = new XMLHttpRequest()
 var shakes_request = new XMLHttpRequest()
+var shakes_request_url = 'https://api.funtranslations.com/translate/shakespeare.json'
 var wiki_text
 var article
 
@@ -15,6 +17,17 @@ function get_article_html(request) {
     let wiki_json
     wiki_json = JSON.parse(request.response)
     return(wiki_json.parse.text["*"])
+}
+
+function get_shakes_html(request) {
+    let p 
+    p = document.createElement('p')
+    
+    let shakes_json
+    shakes_json = JSON.parse(request.response)
+    p.innerText = shakes_json.contents.translated
+
+    return p
 }
 
 function remove_thumbs(div) {
@@ -32,6 +45,12 @@ function remove_thumbs(div) {
 function get_wiki_text(paragraph) {
     let p_text = paragraph.innerText
     return p_text
+}
+
+function make_shakes_url(text, base_url) {
+    let uri = encodeURI(wiki_text)
+    let url = base_url + '?text=' + uri
+    return url
 }
 
 // AJAX Listeners
@@ -59,13 +78,16 @@ wiki_request.onreadystatechange = () => {
 
             // get wiki text and send to translator
 
-            const p = article.querySelector('.mw-parser-output p')
+            let p = article.querySelector('.mw-parser-output p')
             wiki_text = get_wiki_text(p)
+
+            shakes_request_url = make_shakes_url(wiki_text, shakes_request_url)
+            shakes_request.open('POST', shakes_request_url)
+            shakes_request.send()
 
         } else { 
 
-            console.log("wiki xhr status: " + wiki_request.status)
-            
+            console.log("wiki xhr status: " + wiki_request.status)            
             wiki_output.innerHTML = '<p>There was a problem with the data from Wikipedia</p>'
        
         } // end (status === 200)
@@ -76,7 +98,7 @@ shakes_request.onreadystatechange = () => {
     if (shakes_request.readyState < 4) {
 
         console.log("shakes xhr readyState: " + shakes_request.readyState)
-        wiki_output.innerHTML = '<p>Request sent.</p><p> Waiting for response from FunTranslations server.</p>'
+        shakes_output.innerHTML = '<p>Request sent.</p><p> Waiting for response from FunTranslations server.</p>'
     
     } else if (shakes_request.readyState === 4) {
 
@@ -84,12 +106,18 @@ shakes_request.onreadystatechange = () => {
 
         if (shakes_request.status == 200) {
 
-            console.log("shakes xhr status: " + shakes_request.status)
+            // Process FunTranslations response and post to shakes-output
 
+            console.log("shakes xhr status: " + shakes_request.status)
+            shakes_output.innerHTML = ''
+            let shakes_p 
+            shakes_p = get_shakes_html(shakes_request)            
+            shakes_output.appendChild(shakes_p)
+            
         } else {
 
             console.log("shakes xhr status: " + shakes_request.status)
-            wiki_output.innerHTML = '<p>There was a problem with the data from FunTranslations.</p>'
+            shakes_output.innerHTML = '<p>There was a problem with the data from FunTranslations.</p>'
 
         } // end (status === 200)
     } // end (readyState === 4)
